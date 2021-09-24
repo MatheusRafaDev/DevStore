6fde77655e6ab44c567b1cfa951e6f59328fb10f
MatheusRafaDev <90002960+MatheusRafaDev@users.noreply.github.com>
2021-09-23 21:25:25 -0300

    Update Api.js

--- react/site/src/services/Api.js
+++ react/site/src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://devstorezap.herokuapp.com/'
+    baseURL: 'https://devstorezap.herokuapp.com'
 })
 
 export default class Api {
